c36019736a19ddca8ea0bad439368d2435473384
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-07-01 10:05:17 -0500

    [ADD] client status

--- database/core/entities/client.entity.ts
+++ database/core/entities/client.entity.ts
@@ -1,12 +1,16 @@
-import { Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn, Column } from "typeorm"
 import { BaseEntity } from "./shared/base.entity"
 import { User } from "./user.entity"
+import { ClientStatusEnum } from "../enums";
 
 @Entity()
 export class Client extends BaseEntity {
 
     @PrimaryGeneratedColumn('uuid')
     id: string;
+
+    @Column({ type: 'enum', enum: ClientStatusEnum })
+    status: ClientStatusEnum;
 
     @OneToOne(() => User, (user) => user.client)
     @JoinColumn()
